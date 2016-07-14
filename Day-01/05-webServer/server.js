2a51dc5f425ff18707b01fc8528943c34fad1706
Magesh <tkmagesh77@gmail.com>
2016-07-14 16:34:48 +0530

    Day-01 Complete

--- Day-01/05-webServer/server.js
+++ Day-01/05-webServer/server.js
@@ -1,19 +1,61 @@
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+var querystring = require('querystring');
+
+var calculator = require('./calculator');
+
+var staticExtns = ['.html', '.js', '.css', '.ico', '.jpg', '.png', '.json', '.txt', '.xml'];
+
+function isStatic(resource){
+	var resExtn = path.extname(resource);
+	return staticExtns.indexOf(resExtn) !== -1;
+}
 
 var server = http.createServer(function(req, res){
-	console.log('a new connection is established - ', req.url);
-	res.write('<h1>Welcome to node web server</h1>');
-	res.end();
+	console.log(req.url);
+	var urlObj = url.parse(req.url);
+	if (isStatic(urlObj.pathname)){
+		var resourcePath = path.join(__dirname, urlObj.pathname)
+		if (!fs.existsSync(resourcePath)){
+			res.statusCode = 404;
+			res.end();
+			return;
+		}
+		var stream = fs.createReadStream(resourcePath);
+		stream.pipe(res);	
+	} else if (urlObj.pathname === '/calculator' && req.method === 'GET'){
+		var data = querystring.parse(urlObj.query);
+		var result = calculator[data.op](parseInt(data.n1,10), parseInt(data.n2,10));
+		res.write(result.toString());
+		res.end();
+	} else if (urlObj.pathname === '/calculator' && req.method === 'POST'){
+		var inputData = '';
+		req.on('data', function(chunk){
+			inputData += chunk;
+		});
+		req.on('end', function(){
+			console.log('req body ->', inputData);
+			var data = querystring.parse(inputData);
+			var result = calculator[data.op](parseInt(data.n1,10), parseInt(data.n2,10));
+			res.write(result.toString());
+			res.end();
+		})
+	} else {
+		res.statusCode = 404;
+		res.end();
+	}
+	
+
 });
 
 server.listen(8080);
 
-/*
-req.url
-if exists
-	read and send to the client
-else 
-	res.statusCode = 404;
-	res.end()
 
+/*
+staticServer.js (if block)
+dataParser.js (everything 'parse')
+calculatorHandler.js (call to calculator)
+notFoundHandler.js (404)
 */
